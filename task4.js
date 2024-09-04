f830ede30dd2a71fd7032f620061cc170d3e902d
jui9797 <jannatjui@414gmail.com>
2024-09-05 00:34:33 +0600

    first commit

---
+++ task4.js
@@ -0,0 +1,22 @@
+/**
+ * Task-4
+Count the number of properties.
+
+Input:
+
+let student = {
+    name: 'Ariana Grande',
+    age: 21,
+    city: 'Gaibandha',
+    isStudent: true
+};
+ */
+
+let student = {
+    name: 'Ariana Grande',
+    age: 21,
+    city: 'Gaibandha',
+    isStudent: true
+};
+let count =Object.keys(student).length;
+console.log(count);
